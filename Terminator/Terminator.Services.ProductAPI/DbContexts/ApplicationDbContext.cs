42e3c2119dcbd01d553c5a107f75a2dcf31a0448
Denis-PC <denis.pptx@gmail.com>
2023-10-21 22:27:42 +0300

    db configuration

---
+++ Terminator/Terminator.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Terminator.Services.ProductAPI.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+}
