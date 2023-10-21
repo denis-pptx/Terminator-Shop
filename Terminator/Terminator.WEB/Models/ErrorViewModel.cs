c075c93cc12f4028d6c4c6e4ceeb02ac1dcfcc4e
Denis-PC <denis.pptx@gmail.com>
2023-10-21 20:53:50 +0300

    created template projects

---
+++ Terminator/Terminator.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Terminator.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
